d6998523e6eb489e3950c7b248e9e2b909ab5d82
Prabhjot Singh <prabhjots5498@gmail.com>
2024-04-14 22:11:25 +0530

    Added JS

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {
+    updateProgressBar()
+};
+
+function updateProgressBar() {
+    const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById('filled').style.width = scrolled + "%";
+}
